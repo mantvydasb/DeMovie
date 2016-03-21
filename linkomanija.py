8a36a438f6be287f9f480b93f844b9c7f6080a9c
mantvydas <mantvydo@gmail.com>
2016-03-21 19:46:40 +0000

    added colours to make text stand out

--- linkomanija.py
+++ linkomanija.py
@@ -6,6 +6,16 @@ import conf
 import results_parser
 
 
+class Colours:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Torrent:
     title       = ''
     description = ''
@@ -108,7 +118,7 @@ class Linkomanija():
             maxLinks = results_parser.MAX_LINKS
             self.searchRecentTorrentsByQuery(seriesTitle)
 
-            print("[*] " + seriesTitle)
+            print(Colours.OKGREEN + "[*] " + seriesTitle + Colours.ENDC)
             for torrentLink in self.searchResultsParser.parsedTorrentsLinks[index * maxLinks : (index + 1) * maxLinks]:
                 print(linkomanija.baseUrl + torrentLink)
             print("\n")
